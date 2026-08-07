4019b322191888504bbdc75df59ce07ea604240c
Beverly Li <beverlyli2022@gmail.com>
2026-08-07 10:21:23 -0700

    Add build_exp_folder function to create experiment paths

--- config.py
+++ config.py
@@ -19,3 +19,10 @@ EMBEDDING_BATCH_SIZE = 2000
 LOOKAHEAD_DEGREE = 8
 
 CIFAR10_CLASS_NAMES = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
+def build_exp_folder(mode):  #mode: conditional/unconditional
+    return (
+        f"{RESULTS_DIR}/{mode}_n{NUM_PRIVATE_IMAGES}"
+        f"_iter{NUM_ITERATIONS}_syn{NUM_SYNTHETIC_SAMPLES}"
+        f"_H{HISTOGRAM_THRESHOLD}_sigma{NOISE_MULTIPLIER:.2f}"
+    )
